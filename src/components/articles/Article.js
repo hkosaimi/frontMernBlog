0a92e35e0296a8f779a0e74773993ac2fa9acbf2
9haki <125345107+hkosaimi@users.noreply.github.com>
2024-02-08 09:37:51 +0300

    Update Article.js

--- src/components/articles/Article.js
+++ src/components/articles/Article.js
@@ -9,7 +9,7 @@ const Article = () => {
 
   useEffect(() => {
     // Fetch article data based on the ID from the server
-    fetch(`https://backend-mern-blog.vercel.app
+    fetch(`https://mernback-875f.onrender.com
 /api/blogs/articles/${id}`)
       .then((response) => response.json())
       .then((data) => setArticle(data))
